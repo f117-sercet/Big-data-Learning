25126cc9cd820ca1c9885d0bdaad96b74a805846
suki <15159885771@163.com>
2022-03-22 18:45:03 +0800

    遍历文件

--- Handoop/HdfsClientDemo/src/main/java/com/atguigu/hdfs/HdfsClient.java
+++ Handoop/HdfsClientDemo/src/main/java/com/atguigu/hdfs/HdfsClient.java
@@ -1,9 +1,8 @@
 package com.atguigu.hdfs;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,6 +12,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class HdfsClient {
 
@@ -77,6 +77,7 @@ public class HdfsClient {
     }
 
     //删除
+    @Test
     public void testRm() throws IOException {
         // 参数解读：参数1：要删除的路径； 参数2 ： 是否递归删除
         // 删除文件
@@ -87,8 +88,53 @@ public class HdfsClient {
 
         // 删除非空目录
         fs.delete(new Path("/jinguo"), true);
-
-
-
     }
+
+    //文件的更名和移动
+    @Test
+    public void testmv() throws IOException {
+
+        // 参数解读：参数1 ：原文件路径； 参数2 ：目标文件路径
+        // 对文件名称的修改
+        //fs.rename(new Path("/input/word.txt"), new Path("/input/ss.txt"));
+
+        // 文件的移动和更名
+        //fs.rename(new Path("/input/ss.txt"),new Path("/cls.txt"));
+
+        // 目录更名
+        fs.rename(new Path("/input"),new Path("/output"));
+    }
+
+    //获取文件详细信息
+     @Test
+    public void fileDetail() throws IOException {
+
+         //获取文件信息
+         RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"), true);
+
+         //    sql="select CLOSE_COST_ORDER as settlementId,UPDATED_DT as settlementDate,CASH as settlementAmt,PURPOSE as purpose,RECIPIENT_COMPANY_ACCOUNT as incomeBankAccount,RECIPIENT_COMPANY_NAME as incomeName, RECIPIENT_COMPANY_BANK as incomeBank,PAY_COMPANY_ACCOUNT as paymentBankAccount, PAY_COMPANY_NAME as paymentName, PAY_COMPANY_BANK as paymentBank , CREATED_BY as createUserName, UPDATED_BY as checkedUserName,'SKGL' as MODULE_TYPE  from JSZX_RECIPIENT_MANAGER where CLOSE_COST_ORDER=:CLOSE_COST_ORDER";bi
+
+         //遍历文件
+         while (listFiles.hasNext()) {
+
+             LocatedFileStatus fileStatus = listFiles.next();
+
+             System.out.println("===============" + fileStatus.getPath() + "====");
+             System.out.println(fileStatus.getPermission());
+             System.out.println(fileStatus.getOwner());
+             System.out.println(fileStatus.getGroup());
+             System.out.println(fileStatus.getLen());
+             System.out.println(fileStatus.getModificationTime());
+             System.out.println(fileStatus.getReplication());
+             System.out.println(fileStatus.getBlockSize());
+             System.out.println(fileStatus.getPath().getName());
+
+             // 获取块信息
+             BlockLocation[] blockLocations = fileStatus.getBlockLocations();
+
+             System.out.println(Arrays.toString(blockLocations));
+         }
+     }
+
+
 }
